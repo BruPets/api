e4a6c68bf1a2ef37fb5bd4125abbe03c889605a1
Dairo García Naranjo <140646236+BruPets@users.noreply.github.com>
2023-10-22 19:10:37 -0500

    updating error handler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,13 +1,4 @@
-import { CustomError } from '../helpers/customError.js'
-
 export const errorHandler = (err, req, res, next) => {
-  if (!(err instanceof CustomError)) {
-    throw new Error('error parameter must be an instance of the Error class')
-  }
-
-  if (isNaN(err.name) || err.name < 100 || err.name > 599) {
-    throw new Error('name property must be a valid HTTP status code')
-  }
-  res.status(err.name).json({ message: err.message })
+  res.status(isNaN(err.name) ? 500 : err.name).json({ message: err.message })
   next(err)
 }
